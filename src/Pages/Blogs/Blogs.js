2cb251261792998e9aa5c3f4a1e555f9b63ce6f2
BrightsLifE <ovi425764@gmail.com>
2022-11-10 00:40:18 +0600

    rechack responsiveness

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -4,7 +4,7 @@ const Blogs = () => {
     return (
         <div className='max-w-screen-xl mx-auto my-20 space-y-6'>
             <h1 className='text-2xl font-bold text-center my-4'>Blogs</h1>
-            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box  lg:w-1/2 sm:w-full m-auto ">
+            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box  w-full sm:w-1/2 m-auto ">
                 <div className="collapse-title text-xl font-medium">
                     Difference between sql and nosql
                 </div>
@@ -12,7 +12,7 @@ const Blogs = () => {
                     <p tabIndex={0}> <span className='text-2xl text-red-500'>SQL</span> is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). <br /> <span className='text-2xl text-red'>NoSQL</span> is a class of DBMs that are non-relational and generally do not use SQL.</p>
                 </div>
             </div>
-            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box w-1/2 m-auto">
+            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box  w-full sm:w-1/2 m-auto">
                 <div className="collapse-title text-xl font-medium">
                     What is JWT, and how does it work?
                 </div>
@@ -20,7 +20,7 @@ const Blogs = () => {
                     <p tabIndex={0}> <span className='text-2xl text-red-500'>JWT</span>  JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP).</p>
                 </div>
             </div>
-            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box w-1/2 m-auto">
+            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box  w-full sm:w-1/2 m-auto">
                 <div className="collapse-title text-xl font-medium">
                     What is the difference between javascript and NodeJS?
                 </div>
@@ -28,7 +28,7 @@ const Blogs = () => {
                     <p tabIndex={0}> JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
                 </div>
             </div>
-            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box w-1/2 m-auto">
+            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box  w-full sm:w-1/2  m-auto">
                 <div className="collapse-title text-xl font-medium">
                     How does NodeJS handle multiple requests at the same time?
                 </div>
--- src/Pages/UserReviews/User.js
+++ src/Pages/UserReviews/User.js
@@ -78,11 +78,12 @@ const User = ({ user, handlerToDelete }) => {
                         <div
                             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none "
                         >
-                            <div className="flex flex-col my-10 mx-auto ">
+                            <div className="flex  my-10 mx-auto ">
 
                                 <div className="border-0 rounded-lg shadow-lg relative flex  w-full bg-white outline-none focus:outline-none">
 
-                                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <div className="items-start p-5 border-b border-solid border-slate-200 rounded-t">
+
                                         <form className="grid grid-cols-6 gap-4">
 
                                             <div className="col-span-6">
@@ -158,33 +159,34 @@ const User = ({ user, handlerToDelete }) => {
                                             </div>
 
                                         </form>
-                                        <button
-                                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                            onClick={() => setShowModal(false)}
-                                        >
-                                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                                ×
-                                            </span>
-                                        </button>
+
+                                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                            <button
+                                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={() => setShowModal(false)}
+                                            >
+                                                Close
+                                            </button>
+                                            <button
+                                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={() => handleUpdate(_id)}
+                                            >
+                                                Save Changes
+                                            </button>
+                                        </div>
                                     </div>
 
-                                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                        <button
-                                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                            type="button"
-                                            onClick={() => setShowModal(false)}
-                                        >
-                                            Close
-                                        </button>
-                                        <button
-                                            className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                            type="button"
-                                            onClick={() => handleUpdate(_id, setShowModal(false))}
-                                        >
-                                            Save Changes
-                                        </button>
-                                    </div>
                                 </div>
+                                <button
+                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                        ×
+                                    </span>
+                                </button>
                             </div>
                         </div>
                         <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
